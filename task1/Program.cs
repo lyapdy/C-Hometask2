cbb4c4a46d9a35dd2c198743b2bce8bb6190b8de
danillyapunov <lyapdy@gmail.com>
2024-06-28 21:39:20 +0700

    Initial commit

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 1: Напишите программу, которая принимает
+// на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+System.Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num % 7 == 0 && num % 23 == 0)
+{
+    System.Console.WriteLine("Число кратно 7 и 23");
+}
+else
+{
+    System.Console.WriteLine("Число некратно 7 и 23");
+}
---
+++ task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 2: Напишите программу, которая принимает
+// на вход координаты точки (X и Y), причём X ≠ 0
+// и Y ≠ 0 и выдаёт номер координатной четверти плоскости,
+// в которой находится эта точка.
+
+System.Console.WriteLine("Введите координаты точки x, y через пробел: ");
+string[] coordinates = Console.ReadLine().Split(' ');
+int x = Convert.ToInt32(coordinates[0]);
+int y = Convert.ToInt32(coordinates[1]);
+if (x == 0)
+    System.Console.WriteLine("Точка находится на оси ординат Y");
+if (y == 0)
+    System.Console.WriteLine("Точка находится на оси абсцисс X");
+else
+{
+    if (x > 0 && y > 0)
+        System.Console.WriteLine("Точка находится в I квадранте");
+    if (x < 0 && y > 0)
+        System.Console.WriteLine("Точка находится во II квадранте");
+    if (x < 0 && y < 0)
+        System.Console.WriteLine("Точка находится в III квадранте");
+    if (x > 0 && y < 0)
+        System.Console.WriteLine("Точка находится в IV квадранте");
+}
